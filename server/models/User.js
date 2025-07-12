71666cb5d95241d91bf1b0ecb0f3d044bc4c9fb6
Badal9924 <badalkr0521bth@gmail.com>
2025-07-12 18:13:01 +0530

    second commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    _id : {
-        type : String,
-        require : true
+    _id: {
+        type: String,
+        require: true
     },
 
-    name : {
-        type : String,
-        require : true
+    name: {
+        type: String,
+        require: true
     },
 
-    email : {
-        type : email,
-        required : true
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        lowercase: true, 
+        trim: true,
     },
 
-    image : {
-        type : String,
-        required : true
+    image: {
+        type: String,
+        required: true
     }
 });
 
-const User = mongoose.model('Users',UserSchema);
+const User = mongoose.model('Users', UserSchema);
 export default User;
